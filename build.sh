2114fe68d40379c7964889983e75228d7634774f
Korzhavin Ivan <korjavin@gmail.com>
2021-01-21 22:29:32 +0300

    Init

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+export PATH="$PATH:/protoc/bin"
+BUFDIR=$(mktemp -d -t buf-XXXXXXXXXX)
+buf build -o image.bin
+protoc --descriptor_set_in=image.bin --go_out=$BUFDIR $(buf ls-files --input image.bin)
+cp -r $BUFDIR/github.com/korjavin/pbuf-example/* ./
+ls -la  $BUFDIR/
+rm -rf $BUFDIR
