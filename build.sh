b7c0e89135f310f062ffdd46a37b7891182f89ea
Tetiuev Pavel <jetexe2@gmail.com>
2021-01-25 18:43:52 +0500

    improve buf support f

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-export PATH="$PATH:/protoc/bin"
-BUFDIR=$(mktemp -d -t buf-XXXXXXXXXX)
-buf build -o image.bin
-protoc --descriptor_set_in=image.bin --go_out=$BUFDIR --go-grpc_out=$BUFDIR  $(buf ls-files --input image.bin)
-cp -r $BUFDIR/github.com/korjavin/pbuf-example/* ./
-rm -rf $BUFDIR
